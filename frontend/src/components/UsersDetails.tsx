99c8567c3aee65a2ae33f41165184b4200cc86f8
VenkataSrimanNarayana <venkata.sriman@iitgn.ac.in>
2023-11-14 23:43:22 +0530

    user role update

--- frontend/src/components/UsersDetails.tsx
+++ frontend/src/components/UsersDetails.tsx
@@ -3,11 +3,82 @@
 import { DataGrid } from "@mui/x-data-grid";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { Button } from "@mui/material";
+import {
+    Button,
+    Modal,
+    Box,
+    Typography,
+    FormControl,
+    Select,
+    InputLabel,
+    MenuItem,
+} from "@mui/material";
 
 export default function UsersDetails() {
-    const { data: session, status } = useSession();
+    const { data: session, status } = useSession({ required: true });
     const [jsonData, setJsonData] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedRole, setSelectedRole] = useState("");
+    const [selectedId, setSelectedId] = useState("");
+    const [roles, setRoles] = useState([]);
+
+    async function roleChange(id: string) {
+        setSelectedId(id);
+        // Fetch all the roles
+        await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/api/roles/all`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${session?.user.accessToken}`,
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setRoles(data.map((role) => role.name));
+            });
+        // Fetch the user role
+        await fetch(
+            process.env.NEXT_PUBLIC_BACKEND_URL + `/api/users/${id}/role`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${session?.user.accessToken}`,
+                },
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setSelectedRole(data.role);
+            });
+        setModalOpen(true);
+    }
+
+    function handleRoleSubmit() {
+        // Send the request to change the role
+        fetch(
+            process.env.NEXT_PUBLIC_BACKEND_URL +
+                `/api/users/${selectedId}/role`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${session?.user.accessToken}`,
+                },
+                body: JSON.stringify({ newRole: selectedRole }),
+            }
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Invalid Request");
+                }
+                alert("Successfully changed the role");
+                setModalOpen(false);
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    }
 
     const fetchData = async () => {
         try {
@@ -129,15 +200,77 @@ export default function UsersDetails() {
                 </strong>
             ),
         },
+        {
+            field: "roleChange",
+            headerName: "Role Change",
+            width: 120,
+            renderCell: (params: any) => (
+                <strong>
+                    <Button
+                        variant="contained"
+                        onClick={() => {
+                            roleChange(params.row.id);
+                        }}
+                    >
+                        Change
+                    </Button>
+                </strong>
+            ),
+        },
     ];
 
     return (
-        <div style={{ width: "100%" }}>
-            <DataGrid
-                columns={defaultColumns}
-                rows={jsonData}
-                pageSizeOptions={[5, 10, 20, 50, 100]}
-            />
-        </div>
+        <>
+            <Modal
+                open={modalOpen}
+                onClose={() => {
+                    setModalOpen(false);
+                }}
+            >
+                <Box
+                    sx={{
+                        position: "absolute" as "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        minWidth: "10rem",
+                        bgcolor: "background.paper",
+                        border: "2px solid #000",
+                        boxShadow: 24,
+                        p: 4,
+                    }}
+                >
+                    <FormControl
+                        sx={{ marginBottom: "1rem", display: "block" }}
+                    >
+                        <InputLabel id="role-label">Role</InputLabel>
+                        <Select
+                            labelId="role-label"
+                            id="role-select"
+                            value={selectedRole}
+                            onChange={(e) => {
+                                setSelectedRole(e.target.value as string);
+                            }}
+                        >
+                            {roles.map((role) => (
+                                <MenuItem key={role} value={role}>
+                                    {role}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <Button variant="contained" onClick={handleRoleSubmit}>
+                        Submit
+                    </Button>
+                </Box>
+            </Modal>
+            <div style={{ width: "100%" }}>
+                <DataGrid
+                    columns={defaultColumns}
+                    rows={jsonData}
+                    pageSizeOptions={[5, 10, 20, 50, 100]}
+                />
+            </div>
+        </>
     );
 }
